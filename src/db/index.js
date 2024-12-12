e4f81adbecd753fe510f99fb2dc39b1bdcfe6fb7
vikashgoswami30 <109417405+vikashgoswami30@users.noreply.github.com>
2024-12-12 22:55:52 +0530

    add npm files and connect db

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
 const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MONGODB connection FAILED ", error);
-        process.exit(1)
-    }
-}
+  try {
+    console.log(process.env.MONGO_URI)
+    const connectInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`); // Add a `/` between URI and DB_NAME
+    console.log(`\n MongoDB Connected! DB host: ${connectInstance.connection.host}`); // Use backticks for string interpolation
+  } catch (error) {
+    console.error(`Error: ${error.message}`); // Log a clearer error message
+    process.exit(1);
+  }
+};
 
-export default connectDB
+export default connectDB;
